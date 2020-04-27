7b9a9edd96e1ece2ac8dbc693c8e111ba16ed0bd
Stinji <stinjigaming@gmail.com>
2020-04-27 18:45:27 +0200

    Further development of the style button functions in script.js

--- script.js
+++ script.js
@@ -1,9 +1,55 @@
 const gridContainer = document.querySelector('.grid-container');
 const newGrid = document.getElementById('new-grid');
 const styleMenu = document.getElementById('style-menu');
+const btnsContainer = document.getElementById('style-container');
 let styleChoice = 'random';
 
 createGrid(16);
+
+function clearGrid() {
+    while (gridContainer.lastElementChild) {
+        gridContainer.removeChild(gridContainer.lastElementChild);
+    }
+}
+
+function createGrid(gridSize) {
+    for (i = 0; i < gridSize ** 2; i++) {
+        const square = document.createElement('div');
+        square.setAttribute('style', `height: ${100 / gridSize}%; width: ${100 / gridSize}%; border: 1px solid #d1d1d1;`);
+        gridContainer.appendChild(square);
+    }
+    addSelectedStyle();
+}
+
+function promptSize() {
+    let gridSize = prompt('Enter an integer between 1 and 100:');
+    if (gridSize === null) {
+        alert('Cancelled');
+    } else if (gridSize < 1 || gridSize > 100 || gridSize % 1 !== 0 || !parseInt(gridSize)) {
+        alert('Please only enter an integer between 1 and 100');
+        promptSize();
+    } else {
+        clearGrid();
+        createGrid(gridSize);
+    }
+}
+
+newGrid.addEventListener('click', () => { promptSize() });
+
+styleMenu.addEventListener('click', () => {
+    if (btnsContainer.style.display === 'none' || btnsContainer.style.display === '') {
+        btnsContainer.style.display = 'flex';
+    } else {
+        btnsContainer.style.display = 'none';
+    }
+});
+
+document.addEventListener('click', (e) => {
+    if (e.target.id === 'style-container' || e.target.offsetParent.id === 'style-container' || e.target.id === 'style-menu') {
+        return
+    }
+    btnsContainer.style.display = 'none';
+});
 
 function addSelectedStyle() {
     if (styleChoice === 'random') {
@@ -33,55 +79,3 @@ function addSelectedStyle() {
         }));
     }
 }
-
-function createGrid(gridSize) {
-    for (i = 0; i < gridSize ** 2; i++) {
-        const square = document.createElement('div');
-        square.setAttribute('style', `height: ${100 / gridSize}%; width: ${100 / gridSize}%; border: 1px solid #d1d1d1;`);
-        gridContainer.appendChild(square);
-    }
-    addSelectedStyle();
-}
-
-function clearGrid() {
-    while (gridContainer.lastElementChild) {
-        gridContainer.removeChild(gridContainer.lastElementChild);
-    }
-}
-
-function promptSize() {
-    let gridSize = prompt('Enter an integer between 1 and 100:');
-    if (gridSize === null) {
-        alert('Cancelled');
-    } else if (gridSize < 1 || gridSize > 100 || gridSize % 1 !== 0 || !parseInt(gridSize)) {
-        alert('Please only enter an integer between 1 and 100');
-        promptSize();
-    } else {
-        clearGrid();
-        createGrid(gridSize);
-    }
-}
-
-newGrid.addEventListener('click', () => { promptSize() });
-
-function changeStyle(styleChoice, e) {
-    if (styleChoice === 'solid') {
-        return e.target.style.background = 'black';
-    } else if (styleChoice === 'random') {
-        console.log('random' + styleChoice);
-    } else if (styleChoice === 'grayscale') {
-        return console.log(e.target.style.cssText);
-    } else {
-        console.log('eraser' + styleChoice);
-    }
-}
-
-styleArray = Array.prototype.slice.call(document.querySelector('.container-left').children);
-styleArray.forEach(child => {
-    child.addEventListener('click', (e) => {
-        styleChoice = e.target.id;
-        changeStyle(styleChoice);
-    })
-});
-
-//switch statement styleChoice
