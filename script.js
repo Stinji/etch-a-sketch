eab86994a10d3f88a0a5946e826cc0074c04b132
Stinji <stinjigaming@gmail.com>
2020-04-29 15:32:31 +0200

    Finished left container buttons functionality in script.js

--- script.js
+++ script.js
@@ -3,8 +3,22 @@ const newGrid = document.getElementById('new-grid');
 const styleMenu = document.getElementById('style-menu');
 const btnsContainer = document.getElementById('style-container');
 let styleChoice = 'solid';
+let applyStyle = 'mouseenter';
 
-createGrid(16);
+let handler = function (e) {
+    if (styleChoice !== 'grayscale' && e.target.style.opacity !== '1') e.target.style.opacity = '';
+    if (styleChoice === 'random') e.target.style.background = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
+    else if (styleChoice === 'eraser') e.target.style.background = '#fff';
+    else {
+        e.target.style.background = '#000';
+        if (styleChoice === 'grayscale') {
+            let currentLevel = Number(e.target.style.opacity);
+            if (currentLevel === 1) return;
+            e.target.style.opacity = currentLevel += 0.1;
+        }
+    }
+    removeEventListener(applyStyle, handler);
+}
 
 function clearGrid() {
     while (gridContainer.lastElementChild) {
@@ -15,11 +29,13 @@ function clearGrid() {
 function createGrid(gridSize) {
     for (i = 0; i < gridSize ** 2; i++) {
         const square = document.createElement('div');
-        square.setAttribute('style', `height: ${100 / gridSize}%; width: ${100 / gridSize}%;`);
+        square.setAttribute('style', `height: ${100 / gridSize}%; width: ${100 / gridSize}%; opacity: 1`);
         gridContainer.appendChild(square);
     }
-    addSelectedStyle(styleChoice);
+    addSelectedStyle();
 }
+
+createGrid(16);
 
 function promptSize() {
     let gridSize = prompt('Enter an integer between 1 and 50:');
@@ -36,6 +52,27 @@ function promptSize() {
 
 newGrid.addEventListener('click', () => { promptSize() });
 
+function addSelectedStyle() {
+    gridContainer.childNodes.forEach(square => {
+        if (square.style.opacity === '1') square.style.opacity = '';
+        square.addEventListener(applyStyle, handler);
+    });
+}
+
+document.querySelector('.container-left').childNodes.forEach(child => child.addEventListener('click', (e) => {
+    styleChoice = e.target.id;
+    addSelectedStyle();
+}));
+
+document.querySelector('.container-right').childNodes.forEach(child => child.addEventListener('click', (e) => {
+    if (e.target.id === 'trash') {
+        gridContainer.childNodes.forEach(square => square.style.background = '#fff');
+    }
+    else {
+        console.log('not trash');
+    }
+}));
+
 styleMenu.addEventListener('click', () => {
     if (btnsContainer.style.display === 'none' || btnsContainer.style.display === '') {
         btnsContainer.style.display = 'flex';
@@ -50,37 +87,3 @@ document.addEventListener('click', (e) => {
     }
     btnsContainer.style.display = 'none';
 });
-
-document.querySelector('.container-left').childNodes.forEach(child => child.addEventListener('click', (e) => {
-    styleChoice = e.target.id;
-    return addSelectedStyle(styleChoice);
-}));
-
-function addSelectedStyle(styleChoice) {
-
-}
-
-function addSelectedStyle(styleChoice) {
-    if (styleChoice === 'random') {
-        gridContainer.childNodes.forEach(child => child.addEventListener('mouseenter', (e) => {
-            e.target.style.background = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
-        }));
-    } else if (styleChoice === 'grayscale') {
-        gridContainer.childNodes.forEach(child => child.addEventListener('mouseenter', (e) => {
-            e.target.style.background = '#000';
-            let currentLevel = Number(e.target.style.opacity);
-            if (currentLevel === 1) {
-                return;
-            }
-            e.target.style.opacity = currentLevel + 0.1;
-        }));
-    } else if (styleChoice === 'eraser') {
-        gridContainer.childNodes.forEach(child => child.addEventListener('mouseenter', (e) => {
-            e.target.style.background = '#fff';
-        }));
-    } else {
-        gridContainer.childNodes.forEach(child => child.addEventListener('mouseenter', (e) => {
-            e.target.style.background = '#000';
-        }));
-    }
-}
